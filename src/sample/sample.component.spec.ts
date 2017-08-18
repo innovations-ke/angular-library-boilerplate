6218b9be89c377f1b67735a4def621f060d35f55
john.muthurimi <john.muthurimi@softwaregroup-bg.com>
2017-08-18 15:16:01 +0300

    include documentation

--- src/sample/sample.component.spec.ts
+++ src/sample/sample.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By }              from '@angular/platform-browser';
 import { DebugElement }    from '@angular/core';
+import {} from 'jasmine';
 
 import { SampleComponent } from './sample.component';
 
