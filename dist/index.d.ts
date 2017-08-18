7aba35a438dd4d9df9360b4dafbb1a58c0e8df11
john.muthurimi <john.muthurimi@softwaregroup-bg.com>
2017-08-18 14:41:29 +0300

    move sample code to a folder

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,8 @@
 import { ModuleWithProviders } from '@angular/core';
-export * from './sample.component';
-export * from './sample.directive';
-export * from './sample.pipe';
-export * from './sample.service';
+export * from './sample/sample.component';
+export * from './sample/sample.directive';
+export * from './sample/sample.pipe';
+export * from './sample/sample.service';
 export declare class SampleModule {
     static forRoot(): ModuleWithProviders;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SampleComponent } from './sample.component';
-import { SampleDirective } from './sample.directive';
-import { SamplePipe } from './sample.pipe';
-import { SampleService } from './sample.service';
+import { SampleComponent } from './sample/sample.component';
+import { SampleDirective } from './sample/sample.directive';
+import { SamplePipe } from './sample/sample.pipe';
+import { SampleService } from './sample/sample.service';
 
-export * from './sample.component';
-export * from './sample.directive';
-export * from './sample.pipe';
-export * from './sample.service';
+export * from './sample/sample.component';
+export * from './sample/sample.directive';
+export * from './sample/sample.pipe';
+export * from './sample/sample.service';
 
 @NgModule({
   imports: [
