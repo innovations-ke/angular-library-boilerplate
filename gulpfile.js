d0cb209b127b5affba4c6f3560c555d3d3cf1884
SOFTWARE-GROUP\john.muthurimi <john.muthurimi@softwaregroup.com>
2017-09-20 17:53:53 +0300

    changes library structure

--- gulpfile.js
+++ gulpfile.js
@@ -131,7 +131,7 @@ gulp.task('rollup:umd', function () {
       // The name to use for the module for UMD/IIFE bundles
       // (required for bundles with exports)
       // See "name" in https://rollupjs.org/#core-functionality
-      name: 'angular-library-boilerplate',
+      name: 'innovations-ke-angular-library-boilerplate',
 
       // See "globals" in https://rollupjs.org/#core-functionality
       globals: {
@@ -139,7 +139,7 @@ gulp.task('rollup:umd', function () {
       }
 
     }))
-    .pipe(rename('angular-library-boilerplate.umd.js'))
+    .pipe(rename('innovations-ke-angular-library-boilerplate.umd.js'))
     .pipe(gulp.dest(distFolder));
 });
 
