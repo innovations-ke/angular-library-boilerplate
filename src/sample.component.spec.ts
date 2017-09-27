d19677f218b73789a8e1e0b42e87bdc75f8c33a6
SOFTWARE-GROUP\john.muthurimi <john.muthurimi@softwaregroup.com>
2017-09-27 10:08:51 +0300

    include changes on library naming

--- src/sample.component.spec.ts
+++ src/sample.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By }              from '@angular/platform-browser';
 import { DebugElement }    from '@angular/core';
-
 import { SampleComponent } from './sample.component';
+import {} from 'jasmine';
 
 describe('SampleComponent', () => {
 
